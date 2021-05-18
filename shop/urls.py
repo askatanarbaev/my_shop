94ff31b05f83320927c7896766c7578c54e7a944
askatanarbaev <askatanarbaevchik@gmail.com>
2021-05-19 00:19:47 +0600

    Listview

--- shop/urls.py
+++ shop/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+from .views import CategoryListView
+
+urlpatterns = [
+    # path('', post_list, name='home'),
+    path('', CategoryListView.as_view(), name='category'),
+]
--- shop/views.py
+++ shop/views.py
@@ -1,1 +1,21 @@
 from django.shortcuts import render
+from .models import Category
+from django.views.generic.list import ListView
+from django.utils import timezone
+
+def post_list(request):
+    all_models = Category.objects.all()
+    return render(request, "home.html", locals())
+
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'home.html'
+    context_object_name = 'category'
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        return context 
+
+
+    
