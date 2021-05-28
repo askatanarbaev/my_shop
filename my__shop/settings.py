4b5cf2c3f25da58ea1ef5b0ed495adc93daf443f
askatanarbaev <askatanarbaevchik@gmail.com>
2021-05-28 16:34:16 +0600

    celery

--- my__shop/settings.py
+++ my__shop/settings.py
@@ -140,3 +140,13 @@ STATICFILES_DIRS = (
 )
 
 CART_SESSION_ID = 'cart'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# CELERY STUFF
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Africa/Nairobi'
